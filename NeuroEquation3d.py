271a2733ecc2c63aacb163b306e9022a83c2b4fd
robinfissum <robin-fissum@hotmail.com>
2022-11-10 15:31:44 +0100

    TBD

--- NeuroEquation3d.py
+++ NeuroEquation3d.py
@@ -1,6 +1,7 @@
 import numba as nb
 import numpy as np
-from PhysicalConstants import *
+import math
+import PhysicalConstants
 from pde import FieldCollection, PDEBase, PlotTracker, ScalarField, CartesianGrid, MemoryStorage, movie
 
 
@@ -8,57 +9,65 @@ class NeurotransmitterPDE(PDEBase):
     """Coupled equation for neurotransmitter-receptor reaction in 3D
     n, r, rn denote the ScalarField (s) representing free neurotransmitter concentration,
     free receptor concentration and bound receptor-neurotransmitter pairs concentration,
-    respectively.
+    respectively, in dimensionless forms.
     """
 
-    def __init__(self, epsilon, epsilon_A, eta, eta_A, bc="auto_periodic_neumann", t_0, rs_c_N_3d, rs_c_R_3d):
+    def __init__(self, t_max, bc, verbose=False):
+        if bc is None:
+            raise TypeError('Specify boundary condition.')
         super().__init__()
-        self.epsilon = epsilon
-        self.epsilon_A = epsilon_A
-        self.eta = eta
-        self.eta_A = eta_A
         self.bc = bc  # boundary condition
-        self.initial_free_receptors = None
-        self.t_0=t_0
-        self.rs_c_N_3d=rs_c_N_3d
-        self.rs_c_R_3d=rs_c_R_3d
+        self.t_max = t_max
+        self.epsilon = PhysicalConstants.epsilon
+        self.eta = PhysicalConstants.eta
+        self.t0 = PhysicalConstants.t_0
+        self.initial_free_dimensionless_neurotransmitters = PhysicalConstants.N_initial_dimensionless
+        self.initial_free_dimensionless_receptors = PhysicalConstants.R_initial_dimensionless
+        self.verbose = verbose  # print information if python implementation is used.
 
     def get_initial_state(self, grid):
-        """prepare a useful initial state"""
-        # Note: passing data=1 below sets the initial value equal to 1 everywhere for the scalar field
+        """prepare a useful initial state
+        F = ScalarField(grid, data=0, label="") sets the value equal to zero everywhere
+        F.insert(point, amount=7) sets the integrated value equal to 7 over the box containing the point 'point'
+        inside the grid. Thus, point are the coordinate, and not the indices in the grid.
+        Note: If you are using insert K times, then amount must be replaced by:
+        # amount=self.initial_free_dimensionless_neurotransmitters / K
+        """
         n = ScalarField(grid, data=0, label="Field $c_{N}$")
-        n.insert(point=np.array([20, 20, 39]), amount=rs_c_N_3d)
+        n.insert(point=np.array([30, 30, 0.9]), amount=self.initial_free_dimensionless_neurotransmitters)
 
         r = ScalarField(grid, data=0, label="Field $c_{R}$")
-        r.insert(point=np.array([20, 20, 1]), amount=rs_c_R_3d)
+        r.insert(point=np.array([30, 30, 0.9]), amount=self.initial_free_dimensionless_receptors)
 
         # Concentration of bound receptor-neurotransmitter pairs is zero at t=0
         rn = ScalarField(grid, data=0, label="Field $c_{RN}$")
         return FieldCollection([n, r, rn])
 
     def evolution_rate(self, state, t=0):
-        """pure python implementation of the PDE"""
+        """pure python implementation of the PDE
+        This is not run unless the numba overload is below is commented out, and is significantly slower.
+        """
         n, r, rn = state
         rhs = state.copy()
         epsilon = self.epsilon
-        epsilon_A = self.epsilon_A
         eta = self.eta
-        eta_A = self.eta_A
+        if self.verbose and t > 0:
+            print(f'Running. {math.floor(100 * t / self.t_max)}% complete.')
+            print(f'amounts: N={n.integral}, R={r.integral},RN={rn.integral} tot={n.integral+r.integral+2*rn.integral}')
 
-        if r.integral <= 0.5 * self.initial_free_receptors:
-            print('Signal was sent at time t={t}')
-            exit()
-
-        rhs[0] = n.laplace(self.bc) - epsilon_A*r*n + eta_A * rn
-        rhs[1] = -epsilon*r*n + eta*rn
-        rhs[2] = epsilon*r*n - eta*rn
+        rhs[0] = n.laplace(self.bc) - epsilon * r * n + eta * rn
+        rhs[1] = -epsilon * r * n + eta * rn
+        rhs[2] = epsilon * r * n - eta * rn
         return rhs
 
+    # For some reason the numba implementation is unable to interpret the state
     def _make_pde_rhs_numba(self, state):
-        """nunmba-compiled implementation of the PDE"""
-        epsilon, epsilon_A = self.epsilon, self.epsilon_A
-        eta, eta_A = self.eta, self.eta_A
-        laplace = state.grid.make_operator("laplace", bc=self.bc)
+        """nunmba-compiled implementation of the PDE
+        This is faster, since it complies the code into C++ before running.
+        """
+        epsilon = self.epsilon
+        eta = self.eta
+        laplace = state.grid.make_operator('laplace', bc=self.bc)
 
         @nb.jit
         def pde_rhs(state_data, t):
@@ -67,7 +76,7 @@ class NeurotransmitterPDE(PDEBase):
             rn = state_data[2]
 
             rate = np.empty_like(state_data)
-            rate[0] = laplace(n)-epsilon_A * r * n + eta_A * rn
+            rate[0] = laplace(n)-epsilon * r * n + eta * rn
             rate[1] = -epsilon * r * n + eta * rn
             rate[2] = epsilon * r * n - eta * rn
             return rate
@@ -75,45 +84,82 @@ class NeurotransmitterPDE(PDEBase):
         return pde_rhs
 
 
-# Set zero Neumann boundary conditions (particles can't escape domain)
-bc_x_left = {'derivative': 0}
-bc_x_right = {'derivative': 0}
-bc_y_left = {'derivative': 0}
-bc_y_right = {'derivative': 0}
-bc_z_left = {'derivative': 0}
-bc_z_right = {'derivative': 0}
-bc_x = [bc_x_left, bc_x_right]
-bc_y = [bc_y_left, bc_y_right]
-bc_z = [bc_z_left, bc_z_right]
-bc = [bc_x, bc_y, bc_z]
+def main():
+    # Set zero Neumann boundary conditions (particles can't escape domain)
+    bc_x_left = {'derivative': 0}
+    bc_x_right = {'derivative': 0}
+    bc_y_left = {'derivative': 0}
+    bc_y_right = {'derivative': 0}
+    bc_z_left = {'derivative': 0}
+    bc_z_right = {'derivative': 0}
+    bc_x = [bc_x_left, bc_x_right]
+    bc_y = [bc_y_left, bc_y_right]
+    bc_z = [bc_z_left, bc_z_right]
+    boundary_condition = [bc_x, bc_y, bc_z]
 
-# Create the grid
-interval_x = (0, 40)
-interval_y = (0, 40)
-interval_z = (0, 40)
-rectangular_domain = [interval_x, interval_y, interval_z]
-grid_points_per_dimension = [40, 40, 40]
-grid = CartesianGrid(bounds=rectangular_domain, shape=grid_points_per_dimension)
+    '''Create the grid. Because of our scaling, we should have interval_z = (0, 1). 
+    For the synaptic cleft, radius/height = 14.7. Our x and y dimensions should therefore be 
+    at least 2 times the diameter of the axon, i.e. >= 30.  
+    '''
+    interval_x = (0, 60)
+    interval_y = (0, 60)
+    interval_z = (0, 1)
+    rectangular_domain = [interval_x, interval_y, interval_z]
+    grid_points_per_dimension = [60, 60, 20]
+    grid = CartesianGrid(bounds=rectangular_domain, shape=grid_points_per_dimension)
 
-# Set the relevant parameters
-time_max = 0.5
-dt = 0.01
+    # Set the relevant parameters
+    time_max_seconds = math.pow(10, -9)
+    time_max_dimensionless = time_max_seconds / PhysicalConstants.t_0
+    dt_seconds = math.pow(10, -13)
+    dt_dimensionless = dt_seconds / PhysicalConstants.t_0
+    print(f'\nNumber of time steps = {time_max_dimensionless / dt_dimensionless}')
 
-# Create the PDE
-# state represents the scalar fields defined on the grid
-eq = NeurotransmitterPDE(epsilon=epsilon, epsilon_A=epsilon_A, eta=eta, eta_A=eta_A, bc=bc)
-state = eq.get_initial_state(grid)
+    # Give an indication about how likely it is that the scheme is unstable.
+    # Recall that the dimensionless diffusivity now equals 1.
+    cfl_constant = dt_dimensionless * ((grid_points_per_dimension[0] / (interval_x[1] - interval_x[0])) ** 2 +
+                                       (grid_points_per_dimension[1] / (interval_y[1] - interval_y[0])) ** 2 +
+                                       (grid_points_per_dimension[2] / (interval_z[1] - interval_z[0])) ** 2)
+    print(f'The CFL constant is {cfl_constant}')
+    if cfl_constant >= 0.5:
+        print('The scheme will be unstable if an explicit method is used!')
+        print('The issue can be resolved by using SMALLER time step, or using LARGER spatial steps.')
+    else:
+        print('The scheme is likely stable.')
 
-# Simulate the PDE and plot its evolution
-storage = MemoryStorage()
-sol = eq.solve(state, t_range=time_max, dt=dt, tracker=["progress", storage.tracker(0.01)], method='implicit')
-movie(storage=storage, filename='./Animations/NeuroEvolution3D.gif')
+    # Create the PDE
+    # state represents the scalar fields defined on the grid
+    eq = NeurotransmitterPDE(t_max=time_max_dimensionless, bc=boundary_condition, verbose=False)
+    system_state = eq.get_initial_state(grid)
 
-# Get the data at all time steps of all concentrations
-for time, field in storage.items():
-    # Fetch concentrations objects. To get the values as numpy.ndarray , replace by field[i].data
-    concentration_N = field[0]
-    concentration_R = field[1]
-    concentration_RN = field[2]
+    # Store data of the simulation at fixed (dimensionless) time intervals
+    store_data_interval = dt_dimensionless
 
-# Todo: Plot the damn thing in 3d.
+    # Simulate the PDE and plot its evolution
+    storage = MemoryStorage()
+    _ = eq.solve(system_state, t_range=time_max_dimensionless, dt=dt_dimensionless, tracker=["progress", storage.tracker(store_data_interval)], method='implicit')
+
+    # Create an animation of the PDE. This takes quite a while.
+    create_animation = True
+    if create_animation:
+        # Todo: Plot the damn thing in 3d.
+        print('Creating animation.')
+        anim_format = ['gif', 'mp4'][0]
+        movie(storage=storage, filename=f'./Animations/NeuroEvolution3D.{anim_format}')
+
+    # get dimensionless volume of volume elements for each point in the gird
+    volume_box = interval_x[1]*interval_y[1]*interval_z[1]/np.prod(grid_points_per_dimension)
+
+    # Get the data at all time steps of all concentrations
+    for time, field in storage.items():
+        # Fetch concentrations objects. To get the values as numpy.ndarray , replace by field[i].data
+        concentration_N = field[0]
+        concentration_R = field[1]
+        concentration_RN = field[2]
+        if volume_box * np.sum(concentration_R.data) < 0.5 * PhysicalConstants.R_initial_dimensionless:
+            print(f'Signal was sent at dimensionless time t={time}')
+            print(f'That is, the signal was sent after t={time * PhysicalConstants.t_0}seconds.')
+
+
+if __name__ == '__main__':
+    main()
--- PhysicalConstants.py
+++ PhysicalConstants.py
@@ -1,70 +1,76 @@
 import math
 
 '''
-This script constants the physical and dimensionless constant that we 
+This script constants the physical and dimensionless constants that we 
 need in our mathematical model.
 '''
 
 # Number of elementary particles in one mole
-mole = 6.02214076*math.pow(10, 23)
+mole = 6.02214076 * math.pow(10, 23)
 
-# height of synaptic cleft in meters 
-h = 15 * math.pow(10, -9)
-# radius of synaptic cleft in meters
-r = 0.22 * math.pow(10, -6)
-# area of synaptic cleft cross-section
+# Height of synaptic cleft in meters
+h = 1.5 * math.pow(10, -8)
+# Radius of synaptic cleft in meters
+r = 2.2 * math.pow(10, -7)
+# Area of synaptic cleft cross-section (Not used below)
 area = math.pi * math.pow(r, 2)
-# volume of synaptic cleft in cube meters
+# Volume of synaptic cleft in cube meters (Not used below)
 V = math.pi * math.pow(r, 2) * h
 
-# initial neurotransmitter quantity
+# Number of free neurotransmitters at t=0
 N_initial = 5000
 
-# initial neurotransmitter quantity in mole
+# Number of free neurotransmitters at t=0, measured in moles
+# Should equal 8.302695335869232e-21
 A_N = N_initial / mole
 
-# density of receptors on the axon membrane in (number / m^2)
+# Density of receptors on the axon membrane in (number / m^2)
 density_receptors = math.pow(10, 15)
 
-# Total number of receptors on the axon membrane
-# should be equal to 152.05308443374597
+# Total number of receptors on the axon membrane = density * area
+# Should be equal to 152.05308443374597
 R_initial = density_receptors * math.pi * math.pow(r, 2)
 
-# Total number of receptors on the axon membrane in mole
+# Total number of receptors on the axon membrane, measured in moles
+# Should be equal to 2.524900869865187e-22
 A_R = R_initial / mole
+
+# Total number of free neurotransmitter plus total number of free receptors at t=0, measured in moles
+# Should be equal to 8.555185422855751e-21
+A_R_N = A_R + A_N
 
 # Diffusivity of neurotransmitters in m^2/s 
 alpha_N = 8 * math.pow(10, -7)
 
-# reaction constant k_{1} in (mol / L)^{-1} s^{-1}
-_k1 = 4 * math.pow(10, 6)
+# Reaction constant k_{1} in L/(mol s)
+k1_ignore = 4 * math.pow(10, 6)
 
-# reaction constant k_{1} in (mol / m^{3}) s^{-1}
+# Reaction constant k_{1} in m^3/(mol s)
 # (We don't like Liters, 1L=(m/10)^3=(1/1000)m^{3})
-k1 = 4 * math.pow(10, 3)
+k1 = k1_ignore * math.pow(10, -3)
 
-# reaction constant k_{-1} in s^{-1}
+# Reaction constant k_{-1} in s^{-1}
 k_1 = 5
 
-# dimensionless constant epsilon
-# should be equal to 4.095294432922354e-06
-epsilon = k1 * A_N *math.pow(h, 2) /(V * alpha_N)
+# Dimensionless constant epsilon
+# Should be equal to 4.219834862960392e-06
+epsilon = k1 * A_R_N * h/(alpha_N * math.pi * math.pow(r, 2))
 
 # Dimensionless constant eta
 # Should be equal to 1.4062500000000004e-09
 eta = k_1 * math.pow(h, 2) / alpha_N
 
-# other constants
-A = A_R / A_N
-epsilon_A = A * epsilon
-eta_A = A * eta
+# Timescale in seconds
+# Should be equal to 2.812500000000001e-10
+t_0 = math.pow(h, 2)/alpha_N
 
-# initial number of dimensionless neurotransmitters [fix dimensions so this makes sense]:
-initial_N_dimensionless = N_initial * A_N / (V * math.pow(h, 3))
-print(initial_N_dimensionless)
+# Initial amount of dimensionless free neurotransmitters (i.e. volume integral over dimensionless cleft)
+# Should be equal to 655.8467811735285
+N_initial_dimensionless = A_N * math.pi * math.pow(r, 2) / (A_R_N * math.pow(h, 2))
 
-# time scale in seconds
-t_0 = 0.00075
+# Initial amount of dimensionless free receptors (i.e. volume integral over dimensionless cleft)
+# Should be equal to 19.944705198675816
+R_initial_dimensionless = A_R * math.pi * math.pow(r, 2) / (A_R_N * math.pow(h, 2))
 
-# print all names and values
+# To see the value of all variables, run:
 # print(globals())
